bdfbf04f9798d0b28d15375aafae4f02a737d63c
Alexey Shmykov <shmykov.dev@gmail.com>
2019-07-01 18:18:59 +0400

    need http

--- CargoTradesTest/CargoTradesTest/Model.cs
+++ CargoTradesTest/CargoTradesTest/Model.cs
@@ -1,16 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace CargoTradesTest
 {
-    public class Model
+    public class Model : INotifyPropertyChanged
     {
-        public string Data { get; set; }
+        private string data;
+
+        public string Data
+        {
+            get => data;
+            set
+            {
+                data = value;
+                OnPropertyChanged();
+            }
+        }
+
         public ICommand SendData { get; set; }
         public ICommand GetData { get; set; }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 
     public class ModelBuilder
@@ -23,19 +45,23 @@ namespace CargoTradesTest
             model.SendData = new Command(() => client.SendData(model.Data));
             model.GetData = new Command(() => model.Data = client.GetData());
 
+            return model;
         }
     }
 
     public class Client
     {
+        //todo: http
+        string v;
+
         public void SendData(string data)
         {
-
+            v = data;
         }
 
         public string GetData()
         {
-
+            return v;
         }
     }
 }
